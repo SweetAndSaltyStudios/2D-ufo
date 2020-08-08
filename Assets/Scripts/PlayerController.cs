7cf0a5d4345559917977b851bd6ec1cd29990c61
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 20:31:59 +0300

    Project cleaning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+   
+    private Rigidbody2D rb2D;
+    private int count;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        count = 0;
+
+        winText.text = "";
+
+        SetCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2D.AddForce(movement * speed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "" + count.ToString();
+
+        if (count >= 8)
+
+            winText.text = "You win!";
+                
+    }
+
+}
